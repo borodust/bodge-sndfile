5c05fee73ed2be1b05def345059244d644a96612
Pavel Korolev <dev@borodust.org>
2016-11-23 02:45:48 +0300

    Unix (debian/ubuntu) library designator

--- libsndfile.lisp
+++ libsndfile.lisp
@@ -3,6 +3,7 @@
 
 (cffi:define-foreign-library libsndfile
   (:darwin (:or "libsndfile.1.dylib" "libsndfile.dylib"))
+  (:unix (:or "libsndfile.so.1" "libsndfile.so"))
   (t (:default "libsndfile")))
 
 
