554c9a054f01a9e5b946b00b197aa9fd09cdfc96
Pavel Korolev <dev@borodust.org>
2019-09-27 23:02:48 +0300

    Migrate to new :claw interface

--- claw.lisp
+++
@@ -1,6 +0,0 @@
-(claw:c-include "bodge_sndfile.h" :bodge-sndfile
-  :in-package (:%sndfile :%sf)
-  :sysincludes (:sndfile-includes)
-  :include-sources ("sndfile.h")
-  :include-definitions ("(sf|SF).?_\\w*")
-  :rename-symbols (claw:by-removing-prefixes "sf_" "SF" "SF_"))
--- libsndfile.lisp
+++
@@ -1,4 +0,0 @@
-(cffi:define-foreign-library sf:libsndfile
-  (:darwin "libsndfile.dylib.bodged")
-  (:unix "libsndfile.so.bodged")
-  (:windows "libsndfile.dll.bodged"))
