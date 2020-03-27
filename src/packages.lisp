8baa32a0c9a33402a7a0bc92884d50d8629d63d9
Pavel Korolev <dev@borodust.org>
2020-03-28 00:10:56 +0300

    Add format parsing

--- src/packages.lisp
+++ src/packages.lisp
@@ -3,7 +3,11 @@
   (:use :cl :claw :alexandria :cffi-c-ref)
   (:export #:with-open-sound-file
            #:with-sound-file-from-stream
+
            #:sound-sample-rate
            #:sound-channels
+           #:sound-format
+           #:sound-frames
+
            #:read-short-samples-into-array
            #:write-short-samples-into-stream))
